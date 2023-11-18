f2e9b73ec7b5324534947918ebab73c15526c337
ygtdnmz <ygtdnmz6@gmail.com>
2023-11-18 17:29:09 +0300

    app.js dosyasına socket için server eklendi

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const http = require("http")
 const fileUpload = require("express-fileupload");
 const helmet = require("helmet");
 const cors = require("cors");
@@ -11,6 +12,8 @@ config();
 loaders();
 
 const app = express();
+const server = http.createServer(app)
+const serverport = process.env.HTTP_SERVER_PORT || 2018
 
 app.use(express.json());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -33,4 +36,8 @@ const PORT = process.env.APP_PORT || 3001
 
 app.listen(PORT, ()=>{
     console.log(`Uygulama ${PORT} portu üzerinden yayında...`)
+})
+
+server.listen(serverport, ()=>{
+  console.log(`server on port : ${serverport}`)
 })
