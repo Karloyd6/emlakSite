3ae5eb3e6b41f4c3fcc09a37d98029049de8b116
ygtdnmz <ygtdnmz6@gmail.com>
2023-11-19 19:23:07 +0300

    image upload işlemlerini çalışır hale getirdim +kod düzenlemesi

--- services/adverts.js
+++ services/adverts.js
@@ -8,59 +8,66 @@ const insert = (advertData) => {
     return adverts.save();
 };
 
-const add_images = (folder,imageData)=>{
 
-    const imageArray = [];
+
+const add_images = async (folder,imageData) => {
+
+    console.log(imageData.advert_images.length)
+
+    const savedImageArray = [];
     const folderPath = path.join(__dirname,"../",`uploads/adverts/${folder}`)
 
+    //! HAVE CURRENT IMAGES ?///////////
+    await Adverts.findOne({_id : folder}).then((find_response)=>{
+        find_response.advert_images.forEach((value)=>{
+            savedImageArray.push(value);
+        })
+    }).catch(err => console.log("findResponseError",err))
+    //! MULTİPLE OR SİNGLE /////////////
     if(imageData.advert_images.length > 1){
-        imageData.advert_images.forEach((image,index) => {
+        imageData.advert_images.forEach((image)=>{
+            const extension = path.extname(image.name);
+            const imgName = `${Math.random()}${extension}`;
+            const imgUrl = {
+                name : imgName,
+                url: `uploads/adverts/${folder}/${imgName}`
+            }
 
-            const extension = path.extname(image.name)
-            const imageName = `${Math.random()}${extension}`
-            const imageSave = path.join(folderPath,imageName)
-            const imageURL = {
-                name : `${imageName}`,
-                url :`uploads/adverts/${folder}/${imageName}`}
-            imageArray.push(imageURL);
+            savedImageArray.push(imgUrl);
 
-            const updatedImages = imageArray
-
-            //! BURALAR İNCELENECEK ////
-            // let advertImages = []
-            // Adverts.findOne({_id : folder}).then((result)=>{
-            //     console.log("result",result.advert_images)
-            //     advertImages=result.advert_images
-                
-            // }).catch(err => console.log("erorr",err))
-            
-            // updatedImages.concat(advertImages)
-
-            //! buraya kadar//////////////
-            
-
-            const sonuc =Adverts.findByIdAndUpdate(folder,{advert_images : updatedImages}).exec()
-
-            console.log(sonuc)
-
-            image.mv(imageSave,(err)=>{
+            //! ADD TO FOLDER //////////
+            const imgLocation = path.join(folderPath,imgName);
+            image.mv(imgLocation,(err)=>{
                 if(err) console.log("err =>",err)
                 console.log('işlem başarılı :>> ' );
             })
-        });
+        })
     }else{
-        console.log("ananı avradını",imageData.advert_images)
-            const imageName = imageData.advert_images.name
-            const imageSave = path.join(folderPath,imageName)
+        const extension = path.extname(imageData.advert_images.name);
+        const imgName = `${Math.random()}${extension}`;
+        const imgUrl = {
+            name : imgName,
+            url : `uploads/adverts/${folder}/${imgName}`
+        }
 
-            
-    
-            imageData.advert_images.mv(imageSave,(err)=>{
-                if(err) console.log("err =>",err)
-                console.log('işlem başarılı :>> ' );
-            })
+        savedImageArray.push(imgUrl)
+
+        //! ADD TO FOLDER //////////
+        const imgLocation = path.join(folderPath,imgName);
+        imageData.advert_images.mv(imgLocation,(err)=>{
+            if(err) console.log("err =>",err)
+            console.log('işlem başarılı :>> ' );
+        })
+
     }
-  return "işlem başarılı"
+    
+
+    //! SAVE DATABASE //////////////////
+    const dbResponse =await Adverts.findByIdAndUpdate({_id : folder}, {advert_images : savedImageArray}).exec()
+
+    console.log("dbResponse",dbResponse)
+    return dbResponse;
+
 }
 
 const listAds = (id)=>{
@@ -81,16 +88,16 @@ const updateAds = (id,updated) => {
 }
 
 const updateImages = (id,updated_images) => {
-    let advertImages = []
-    Adverts.findOne({_id : id}).then((result)=>{
-        console.log("result",result.advert_images)
-        advertImages=result.advert_images
+    // let advertImages = []
+    // Adverts.findOne({_id : id}).then((result)=>{
+    //     console.log("result",result.advert_images)
+    //     advertImages=result.advert_images
         
-    }).catch(err => console.log("erorr",err))
+    // }).catch(err => console.log("erorr",err))
     
-    advertImages.push(updated_images)
+    // advertImages.push(updated_images)
 
-    return Adverts.findOneAndUpdate({_id : id},{advert_images : advertImages});
+    return Adverts.findOneAndUpdate({_id : id},{advert_images : updated_images});
 
 }
 
